d95c6e3d01e9b5f85da966f60a09bb38979e0d80
Yasuaki Shibata <shivaq777@gmail.com>
2019-11-23 12:21:18 +0900

    Change the place to execute activating utility shell

--- set_yourip_to_sg_for_jump_srv/set_yourip_to_sg_for_jump_srv.sh
+++ set_yourip_to_sg_for_jump_srv/set_yourip_to_sg_for_jump_srv.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-UTIL_FILE=util_shell.sh
 
 # Get my own IP address
 MY_IP_ADDRESS=$(curl http://checkip.amazonaws.com/)
@@ -17,9 +16,8 @@ LIST_OF_PARAMS="ParameterKey=$PARAM_KEY_TO_UPDATE,ParameterValue=$MY_IP_ADDRESS/
 # LIST_OF_PARAMS="ParameterKey=$PARAM_KEY_TO_UPDATE,ParameterValue=８/32"
 # LIST_OF_PARAMS="ParameterKey=$PARAM_KEY_TO_UPDATE,ParameterValue=8.8.8.8/32"
 
-
-echo "Allow my jump server security group to access from $LIST_OF_PARAMS in SSH."
-
+# Activate using utility shell
+UTIL_FILE=util_shell.sh
 if [ -f "$UTIL_FILE" ]; then
     echo "$UTIL_FILE is here"
     . util_shell.sh
@@ -28,6 +26,8 @@ else
     cd ..
     . util_shell.sh
 fi
+
+echo "Allow my jump server security group to access from $LIST_OF_PARAMS in SSH."
 
 update_stack_parameter $DEFAULT_PROFILE $STACK_TO_UPDATE $LIST_OF_PARAMS
 
