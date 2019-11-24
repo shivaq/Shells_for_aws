58cacf73ca8e2686b2d3787ba2e4242fee4c7831
Yasuaki Shibata <shivaq777@gmail.com>
2019-11-25 03:14:47 +0900

    Fix path of this shell and modify set aliase file as it should be

--- set_aliases_for_aws/set_aliases_for_aws.sh
+++ set_aliases_for_aws/set_aliases_for_aws.sh
@@ -5,16 +5,6 @@
 
 MFA_ON=true
 
-
-# Activate using utility shell
-REFERENCED_FILE=auto_mfa_for_aws_cli/mfa.sh
-if [ -f "$REFERENCED_FILE" ]; then
-    cp $REFERENCED_FILE ~/auto_mfa_for_aws_cli/mfa.sh
-else
-    cd ..
-    cp $REFERENCED_FILE ~/auto_mfa_for_aws_cli/mfa.sh
-fi
-
 if [ $MFA_ON = true ]; then 
-    alias mfa="source ~/auto_mfa_for_aws_cli/mfa.sh $1 $2"
+    alias mfa="~/.Shells_for_aws/auto_mfa_for_aws_cli/mfa.sh $1 $2"
 fi
