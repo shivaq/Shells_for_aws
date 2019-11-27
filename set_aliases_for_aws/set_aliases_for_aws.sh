1890ac73a803289b489c3dddd6f6bdf90c53991a
Yasuaki Shibata <shivaq777@gmail.com>
2019-11-28 01:48:47 +0900

    Modify variable name

--- set_aliases_for_aws/set_aliases_for_aws.sh
+++ set_aliases_for_aws/set_aliases_for_aws.sh
@@ -7,11 +7,11 @@ MFA_ON=true
 
 setToken() {
     ENV_FILE=~/.Shells_for_aws/export_env_vir_skdjflsdjflseiworueiruowieuruwo
-    touch $EXPORT_ENV_FILE_PATH
+    touch $ENV_FILE
     ~/.Shells_for_aws/auto_mfa_for_aws_cli/mfa.sh $1 $2
     # you can't set environment variables in mfa.sh. So do that here.
     source $ENV_FILE
-    echo "Your creds have been set in your env."
+    echo "Your credentials have been set in your env."
     rm -f $ENV_FILE
 }
 alias mfa=setToken
