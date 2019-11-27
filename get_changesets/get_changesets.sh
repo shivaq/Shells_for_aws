495f19e32e9e3d0f93c1ffa200f80b34d4a422d3
Yasuaki Shibata <shivaq777@gmail.com>
2019-11-28 02:02:44 +0900

    get template diff

--- get_changesets/get_changesets.sh
+++ get_changesets/get_changesets.sh
@@ -5,38 +5,22 @@ STACK_TO_UPDATE=${1:-Ec2}
 # Activate using utility shell
 source ~/.Shells_for_aws/util_shell.sh
 
-# Default profile executing aws cli
+# Get the default profile executing aws cli
 VALUE_TO_FIND="DEFAULT_PROFILE"
 get_value_from_config $VALUE_TO_FIND
 DEFAULT_PROFILE=$ret_value
 
-# get the path of template
+# Get the path of template
 VALUE_TO_FIND="PATH_OF_STACK"
 get_value_from_config $VALUE_TO_FIND
 PATH_OF_STACK=$ret_value
 
-# Path of local cfn template
+# Make the path of local cfn template
 NEW_FILE=$PATH_OF_STACK$STACK_TO_UPDATE.yaml
 
-###########################
-# get a template diff
-###########################
+# Get a template diff
+get_a_template_diff $DEFAULT_PROFILE $STACK_TO_UPDATE $NEW_FILE
 
-# Get the template used in cfn stack
-# strict=False let ctrl characters inside strings
-# jq is difficult to avoid ctrl character error so use python
-current_template="$(\
-            aws cloudformation --profile $DEFAULT_PROFILE \
-            get-template --template-stage Original --stack-name $STACK_TO_UPDATE\
-            | python3 -c "import sys, json; \
-            print(json.load(sys.stdin, strict=False)\
-            ['TemplateBody'])"\
-            )"
-
-echo "$current_template" > diff_file_flskdjfoeriuwoeutwo
-
-diff diff_file_flskdjfoeriuwoeutwo $NEW_FILE
-rm -f diff_file_flskdjfoeriuwoeutwo
 
 DATE=`date +"%m-%d-%H-%M"`
 CSNAME="$STACK_TO_UPDATE-`date +"%m-%d-%H-%M"`-$DEFAULT_PROFILE"
